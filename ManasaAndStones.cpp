e3f3e555fed8f884ff8a4c6d1dd2be50be4247e9
Sunny Kumar <sun42nykumarsk@gmail.com>
2018-09-11 01:06:48 +0530

    Create ManasaAndStones.cpp

---
+++ ManasaAndStones.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    long long int t, a, b, i, c, d, n;
+    cin >> t;
+    
+    while(t--) 
+    {
+        set<int> x;
+        cin >> n >> c >> d;
+        
+        a = min(c,d);
+        b = max(c,d);
+        n--;
+        
+        for (i = 0; i <= n; i++)
+            x.insert(i * b + (n - i) * a);
+        
+        for (auto& it:x) 
+            cout << it << " ";
+        
+        cout << endl;
+    }
+    return 0;
+}
