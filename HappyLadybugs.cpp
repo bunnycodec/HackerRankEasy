2170c1bb00783b09fc3837135b62f2270ed560e0
Sunny Kumar <sun42nykumarsk@gmail.com>
2018-09-12 00:44:07 +0530

    Update HappyLadybugs.cpp

--- HappyLadybugs.cpp
+++ HappyLadybugs.cpp
@@ -1,6 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void check(vector<char> ans)
+{
+    int i, count = 0, flag = 0;
+    
+    for(i=0; i<ans.size(); i++)
+    {
+        if(ans[i] == '_')
+            break;
+
+        if(ans[i] == ans[i+1])
+        {
+            count++;
+            continue;
+        }
+        else
+        {
+            if(count>0)
+            {
+                count=0;
+                continue;
+            }
+            else
+            {
+                flag=1;
+                break;
+            }
+        }
+    }
+
+    if(flag == 1)
+        cout << "NO\n";
+    else
+        cout << "YES\n";
+}
+
 int main()
 {
     int i, t, n;
@@ -18,70 +53,11 @@ int main()
         vector<char> ans2(s.begin(), s.end());
         
         sort(ans.begin(), ans.end());
-        int flag = 0, count = 0;
         
         if(ans[ans.size()-1] == '_')
-        {
-            for(i=0; i<ans.size(); i++)
-            {
-                if(ans[i] == '_')
-                    break;
-
-                if(ans[i] == ans[i+1])
-                {
-                    count++;
-                    continue;
-                }
-                else
-                {
-                    if(count>0)
-                    {
-                        count=0;
-                        continue;
-                    }
-                    else
-                    {
-                        flag=1;
-                        break;
-                    }
-                }
-            }
-
-            if(flag == 1)
-                cout << "NO\n";
-            else
-                cout << "YES\n";
-        }
+            check(ans);
         else
-        {
-            for(i=0; i<ans2.size(); i++)
-            {
-                if(ans2[i] == ans2[i+1])
-                {
-                    count++;
-                    continue;
-                }
-                else
-                {
-                    if(count>0)
-                    {
-                        count=0;
-                        continue;
-                    }
-                    else
-                    {
-                        flag=1;
-                        break;
-                    }
-                }
-            }
-
-            if(flag == 1)
-                cout << "NO\n";
-            else
-                cout << "YES\n";
-        }
-        
+            check(ans2);
     }    
 
     return 0;
